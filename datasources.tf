38f27dfa3ad597cc36c0ed95db779fd56867da59
saadallh <124841208+saadallh@users.noreply.github.com>
2024-09-11 01:30:52 +0200

    Add files via upload

---
+++ datasources.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "server-ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+
+  owners = ["099720109477"] # Canonical
+}
+
